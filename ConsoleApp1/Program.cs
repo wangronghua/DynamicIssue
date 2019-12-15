3e74c99288f566e4542547fb5229c4899ad9fc90
bruce <289022650@qq.com>
2019-12-15 14:58:06 +0800

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,4 +31,27 @@ namespace ConsoleApp1
             Console.WriteLine("结束");
         }
     }
+    public class ProxyDecorator2<T, TClass> : DispatchProxy where T : class where TClass : T, new()
+    {
+        protected T _decorated;
+
+        public static T Create()
+        {
+            var proxy = Create<T, ProxyDecorator2<T, TClass>>();
+            var proxyReal = proxy as ProxyDecorator2<T, TClass>;
+            if (proxyReal == null) throw new Exception("proxyReal报错");
+            proxyReal._decorated = new TClass();
+            return proxy;
+        }
+        private async Task<object> InvokeCoore(MethodInfo targetMethod, object[] args)
+        {
+            dynamic dy = targetMethod.Invoke(_decorated, args);
+            var x = await dy;
+            return dy;
+        }
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            return InvokeCoore(targetMethod, args).Result;
+        }
+    }
 }
